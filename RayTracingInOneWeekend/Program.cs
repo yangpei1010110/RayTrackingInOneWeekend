992685418eb365e4b98126fdca01ac725a4d061f
yangpei1010110 <970387216@qq.com>
2023-04-18 00:39:07 +0800

    clean out file

--- RayTracingInOneWeekend/Program.cs
+++ RayTracingInOneWeekend/Program.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Numerics;
 using System.Text;
 using RayTracingInOneWeekend.Utility;
