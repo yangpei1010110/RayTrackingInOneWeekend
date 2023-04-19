e23624bc9583c3b690424094f94c23e8d1127dbe
yangpei1010110 <970387216@qq.com>
2023-04-19 12:55:28 +0800

    6.5

---
+++ RayTracingInOneWeekend/Utility/Hit/HittableList.cs
@@ -0,0 +1,29 @@
+namespace RayTracingInOneWeekend.Utility.Hit;
+
+public class HittableList : IHittable
+{
+    private readonly List<IHittable> _objects = new();
+
+    public bool Hit(Ray r, float tMin, float tMax, out HitRecord rec)
+    {
+        rec = default(HitRecord);
+        HitRecord tempRec = default(HitRecord);
+        bool hitAnything = false;
+        float closestSoFar = tMax;
+
+        foreach (IHittable obj in _objects)
+        {
+            if (obj.Hit(r, tMin, closestSoFar, out tempRec))
+            {
+                hitAnything = true;
+                closestSoFar = tempRec.T;
+                rec = tempRec;
+            }
+        }
+
+        return hitAnything;
+    }
+
+    public void Clear()            => _objects.Clear();
+    public void Add(IHittable obj) => _objects.Add(obj);
+}
