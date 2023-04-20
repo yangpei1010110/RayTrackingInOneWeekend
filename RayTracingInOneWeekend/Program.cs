d446a997b22f69b3ab073942d2b3bb90fd6ef9b3
yangpei1010110 <970387216@qq.com>
2023-04-21 01:17:32 +0800

    13.1

--- RayTracingInOneWeekend/Program.cs
+++ RayTracingInOneWeekend/Program.cs
@@ -3,8 +3,6 @@ using System.Numerics;
 using System.Text;
 using RayTracingInOneWeekend.Utility;
 using RayTracingInOneWeekend.Utility.Hit;
-using RayTracingInOneWeekend.Utility.Mat;
-using RayTracingInOneWeekend.Utility.Shape;
 
 namespace RayTracingInOneWeekend;
 
@@ -14,33 +12,22 @@ internal class Program
     {
         // Image
         string filePath = Path.GetFullPath(@"image.ppm");
-        float aspectRatio = 16.0f / 9.0f;
-        int imageWidth = 800;
+        float aspectRatio = 3.0f / 2.0f;
+        int imageWidth = 1200;
         int imageHeight = (int)(imageWidth / aspectRatio);
-        int samplesPerPixel = 100;
-        int maxDepth = 50;
+        int samplesPerPixel = 512;
+        int maxDepth = 64;
         StringBuilder sb = new();
 
         // World
-        HittableList world = new();
-
-        IMaterial materialGround = new Lambertian(new Vector3(0.8f, 0.8f, 0.0f));
-        IMaterial materialCenter = new Lambertian(new Vector3(0.1f, 0.2f, 0.5f));
-        IMaterial materialLeft = new Dielectric(1.5f);
-        IMaterial materialRight = new Metal(new Vector3(0.8f, 0.6f, 0.2f), 0.0f);
-
-        world.Add(new Sphere(new Vector3(0, -100.5f, -1), 100, materialGround));
-        world.Add(new Sphere(new Vector3(0, 0, -1), 0.5f, materialCenter));
-        world.Add(new Sphere(new Vector3(-1, 0, -1), 0.5f, materialLeft));
-        world.Add(new Sphere(new Vector3(-1, 0, -1), -0.4f, materialLeft));
-        world.Add(new Sphere(new Vector3(1, 0, -1), 0.5f, materialRight));
+        HittableList world = HittableList.RandomScene();
 
         // Camera
-        Vector3 lookFrom = new(3, 3, 2);
-        Vector3 lookAt = new(0, 0, -1);
+        Vector3 lookFrom = new(13, 2, 3);
+        Vector3 lookAt = new(0, 0, 0);
         Vector3 vUp = new(0, 1, 0);
-        float distToFocus = (lookFrom - lookAt).Length();
-        float aperture = 2.0f;
+        float distToFocus = 10.0f;
+        float aperture = 0.1f;
         Camera camera = new(
             lookFrom,
             lookAt,
@@ -58,6 +45,7 @@ internal class Program
         Vector3[] pixels = new Vector3[imageHeight * imageWidth];
         object lockObj = new();
         int work = 0;
+        Stopwatch sw = Stopwatch.StartNew();
         Parallel.For(0, imageHeight, j =>
         {
             for (int i = 0; i < imageWidth; i++)
@@ -77,24 +65,11 @@ internal class Program
             lock (lockObj)
             {
                 work++;
-                Console.Error.WriteLine($"\rScanLines remaining: {work / (float)imageHeight:P} {imageHeight - work}");
+                Console.WriteLine($"\rScanLines remaining: {work / (float)imageHeight:P} {imageHeight - work} {sw.Elapsed}");
             }
         });
-        // for (int j = imageHeight - 1; j >= 0; j--)
-        // {
-        //     Console.Error.WriteLine($"\rScanLines remaining: {j}");
-        //     for (int i = 0; i < imageWidth; i++)
-        //     {
-        //         for (int s = 0; s < samplesPerPixel; s++)
-        //         {
-        //             float u = (i + Random.Shared.NextSingle()) / (imageWidth - 1);
-        //             float v = (j + Random.Shared.NextSingle()) / (imageHeight - 1);
-        //
-        //             Ray r = camera.GetRay(u, v);
-        //             pixels[j * imageWidth + i] += Tool.RayColor(r, world, maxDepth);
-        //         }
-        //     }
-        // }
+        sw.Stop();
+        Console.WriteLine($"Finish in {sw.Elapsed}");
 
         // write color to file
         for (int j = imageHeight - 1; j >= 0; j--)
--- RayTracingInOneWeekend/Utility/Hit/HittableList.cs
+++ RayTracingInOneWeekend/Utility/Hit/HittableList.cs
@@ -1,3 +1,8 @@
+using System.Numerics;
+using RayTracingInOneWeekend.Utility.Extensions;
+using RayTracingInOneWeekend.Utility.Mat;
+using RayTracingInOneWeekend.Utility.Shape;
+
 namespace RayTracingInOneWeekend.Utility.Hit;
 
 public class HittableList : IHittable
@@ -25,4 +30,57 @@ public class HittableList : IHittable
 
     public void Clear()            => _objects.Clear();
     public void Add(IHittable obj) => _objects.Add(obj);
+
+    public static HittableList RandomScene()
+    {
+        HittableList world = new();
+
+        var groundMaterial = new Lambertian(new Vector3(0.5f, 0.5f, 0.5f));
+        world.Add(new Sphere(new Vector3(0, -1000, 0), 1000, groundMaterial));
+
+        for (int a = -11; a < 11; a++)
+        {
+            for (int b = -11; b < 11; b++)
+            {
+                float chooseMat = Random.Shared.NextSingle();
+                Vector3 center = new(a + 0.9f * Random.Shared.NextSingle(), 0.2f, b + 0.9f * Random.Shared.NextSingle());
+
+                if ((center - new Vector3(4, 0.2f, 0)).Length() > 0.9f)
+                {
+                    if (chooseMat < 0.8f)
+                    {
+                        // diffuse
+                        Vector3 albedo = Tool.RandomVector3() * Tool.RandomVector3();
+                        var sphereMaterial = new Lambertian(albedo);
+                        world.Add(new Sphere(center, 0.2f, sphereMaterial));
+                    }
+                    else if (chooseMat < 0.95f)
+                    {
+                        // metal
+                        Vector3 albedo = Tool.RandomVector3(0.5f, 1f);
+                        float fuzz = Random.Shared.NextFloat(0, 0.5f);
+                        var sphereMaterial = new Metal(albedo, fuzz);
+                        world.Add(new Sphere(center, 0.2f, sphereMaterial));
+                    }
+                    else
+                    {
+                        // glass
+                        var sphereMaterial = new Dielectric(1.5f);
+                        world.Add(new Sphere(center, 0.2f, sphereMaterial));
+                    }
+                }
+            }
+        }
+
+        var material1 = new Dielectric(1.5f);
+        world.Add(new Sphere(new Vector3(0, 1, 0), 1.0f, material1));
+
+        var material2 = new Lambertian(new Vector3(0.4f, 0.2f, 0.1f));
+        world.Add(new Sphere(new Vector3(-4, 1, 0), 1.0f, material2));
+
+        var material3 = new Metal(new Vector3(0.7f, 0.6f, 0.5f), 0.0f);
+        world.Add(new Sphere(new Vector3(4, 1, 0), 1.0f, material3));
+
+        return world;
+    }
 }
