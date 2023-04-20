35044dacf8e618ae3e4288c85405085d53b0e969
yangpei1010110 <970387216@qq.com>
2023-04-20 10:23:39 +0800

    fix

--- RayTracingInOneWeekend/Utility/Color.cs
+++ RayTracingInOneWeekend/Utility/Color.cs
@@ -7,9 +7,8 @@ public static class Color
 {
     public static void WriteColor(StringBuilder sb, Vector3 pixelColor, int samplesPerPixel)
     {
-        float scale = 1.0f / samplesPerPixel;
-        pixelColor *= scale;
-        pixelColor = new Vector3(MathF.Sqrt(pixelColor.X), MathF.Sqrt(pixelColor.Y), MathF.Sqrt(pixelColor.Z));
+        pixelColor *= 1.0f / samplesPerPixel;
+        pixelColor = Vector3.SquareRoot(pixelColor); // gamma correction
         sb.Append((int)(256 * float.Clamp(pixelColor.X, 0f, 0.999f)));
         sb.Append(' ');
         sb.Append((int)(256 * float.Clamp(pixelColor.Y, 0f, 0.999f)));
